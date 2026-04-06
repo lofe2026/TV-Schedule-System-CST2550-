67d37b319dbacb64c178163c2b009f5ac56822c2
mehdi00712 <mbhoyroo246@gmail.com>
2026-04-06 10:44:05 +0400

    updated

--- TVSchedulingSystem.Tests/obj/Debug/net10.0-windows/TVSchedulingSystem.Tests.AssemblyInfo.cs
+++ TVSchedulingSystem.Tests/obj/Debug/net10.0-windows/TVSchedulingSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TVSchedulingSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1047683f3e6cab9297878dd58be885753221e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("TVSchedulingSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TVSchedulingSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
